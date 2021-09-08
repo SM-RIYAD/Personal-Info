037ed8e5268de120ee103cd1dd22ba3bc80527ec
SM-RIYAD <88622536+SM-RIYAD@users.noreply.github.com>
2021-09-09 01:26:48 +0600

    Completed with out oop

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,13 +12,15 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.MessageFormat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 private Button button_insert;
 private TextView text_name;
 private TextView text_age;
 private TextView text_hobby;
 private TextView text_location;
-private CardView cardView1;
+private CardView cardView1,cardReset;
 private EditText editText1;
 private RelativeLayout relative_input;
     @Override
@@ -31,40 +34,11 @@ private RelativeLayout relative_input;
    button_insert = findViewById(R.id.button_insert);
    editText1=findViewById(R.id.edit_text1);
    cardView1 =findViewById(R.id.cardview1);
+   cardReset =findViewById(R.id.card_reset);
    relative_input=findViewById(R.id.relative_input);
    setVisibilityToGone();
-   button_insert.setOnClickListener(new View.OnClickListener() {
-       @Override
-       public void onClick(View view) {
-           if(editText1.getText().toString().isEmpty()){
-
-               Toast.makeText(getApplicationContext(), "Field can't be empty!", Toast.LENGTH_SHORT).show();
-               return;
-           }
-           cardView1.setVisibility(View.VISIBLE);
-           if (text_name.getVisibility() == View.GONE) {
-               text_name.setVisibility(View.VISIBLE);
-               return;
-           }
-
-           if (text_age.getVisibility() == View.GONE) {
-               text_age.setVisibility(View.VISIBLE);
-               return;
-           }
-
-
-          if(text_location.getVisibility()==View.GONE){
-              text_location.setVisibility(View.VISIBLE);
-              return;
-
-          }
-           if (text_hobby.getVisibility() == View.GONE) {
-               text_hobby.setVisibility(View.VISIBLE);
-               relative_input.setVisibility(View.GONE);
-           }
-
-       }
-   });
+   cardReset.setOnClickListener(this);
+   button_insert.setOnClickListener(this);
     }
 
     private void setVisibilityToGone() {
@@ -75,5 +49,74 @@ private RelativeLayout relative_input;
         text_hobby.setVisibility(View.GONE);
 
         cardView1.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v==button_insert){
+
+            if(editText1.getText().toString().isEmpty()){
+                Toast.makeText(getApplicationContext(), "Field can't be empty!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            cardView1.setVisibility(View.VISIBLE);
+            if (text_name.getVisibility() == View.GONE) {
+                ///text_name.setText(editText1.getText().toString());
+                text_name.setText(MessageFormat.format("My name is {0}", editText1.getText().toString()));
+                editText1.setText("");
+                text_name.setVisibility(View.VISIBLE);
+                editText1.setInputType(InputType.TYPE_CLASS_NUMBER);
+                editText1.setHint("Enter age.");
+                return;
+            }
+
+            if (text_age.getVisibility() == View.GONE) {
+                text_age.setVisibility(View.VISIBLE);
+                text_age.setText(MessageFormat.format("I am {0} years old", editText1.getText().toString()));
+                editText1.setText("");
+                text_age.setVisibility(View.VISIBLE);
+                editText1.setInputType(InputType.TYPE_CLASS_TEXT);
+                editText1.setHint("Enter location.");
+                return;
+            }
+
+
+            if(text_location.getVisibility()==View.GONE){
+                text_location.setVisibility(View.VISIBLE);
+                text_location.setText(MessageFormat.format("I live in {0}", editText1.getText().toString()));
+                editText1.setText("");
+                text_location.setVisibility(View.VISIBLE);
+                editText1.setInputType(InputType.TYPE_CLASS_TEXT);
+                editText1.setHint("Enter hobby.");
+                return;
+
+            }
+            if (text_hobby.getVisibility() == View.GONE) {
+                text_hobby.setVisibility(View.VISIBLE);
+
+                text_hobby.setText(MessageFormat.format("I love {0}", editText1.getText().toString()));
+                editText1.setText("");
+
+
+                editText1.setHint("Enter name.");
+                relative_input.setVisibility(View.GONE);
+                cardReset.setVisibility(View.VISIBLE);
+            }
+
+
+
+        }
+        if(v==cardReset){
+
+            setVisibilityToGone();
+            editText1.setText("");
+            editText1.setInputType(InputType.TYPE_CLASS_TEXT);
+            editText1.setHint("Enter name.");
+            cardReset.setVisibility(View.GONE);
+            relative_input.setVisibility(View.VISIBLE);
+
+        }
+
     }
 }
